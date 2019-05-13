e9f289548389d74cd148b8ec29cf09677b1085d1
osmanok <osmanokuyan@outlook.com>
2019-05-14 02:16:27 +0300

    changed database && added gitignore

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var Message = mongoose.model('Message',{
   message : String
 })
 
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+var dbUrl = 'mongodb+srv://admin:admin@cluster0-xrf70.mongodb.net/test?retryWrites=true'
 
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
